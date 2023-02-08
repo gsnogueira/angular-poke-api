d27b98cb1dff0eb177de77b78d65fcbbf3316f6c
Gabriel Nogueira <gs.gamestudy@gmail.com>
2023-02-08 14:44:53 -0300

    include components, bootstrap elements

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PokeListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
